e706e961a709a8ea12b543a7f8cc1b907a9fe219
Nasir Siraj <mdnasir74725@gmail.com>
2021-01-27 12:17:34 +0600

    Initial commit

---
+++ src/main/kotlin/titan/io/ibcslibraryapi/IbcsLibraryApiApplication.kt
@@ -0,0 +1,11 @@
+package titan.io.ibcslibraryapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IbcsLibraryApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<IbcsLibraryApiApplication>(*args)
+}
